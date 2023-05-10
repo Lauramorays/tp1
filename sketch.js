801c38d0c7923818dc21132286b7a0eb6ffe69e8
Lauramorays <lauramoraysrodas@gmail.com>
2023-05-10 11:47:46 -0300

    tp12

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+//las cosas que se cargan antes de iniciar el sketch//
+function preload(){
+  imgfondo = loadImage('imagenes/lienzofondo2.jpg');
+  //trazo del fondo//
+  trazofondo = loadImage('trazos/trazofondo_13.png');
+  mascaratfondo= loadImage('trazos/trazofondo_14_a.png');
+}
+  function setup() {
+createCanvas(windowWidth,windowHeight);
+//fondo//
+image(imgfondo, 0, 0);
+trazofondo.mask(mascaratfondo);
+colorMode(HSB);
+}
+
+function draw() {
+//fondo//
+blendMode(MULTIPLY);
+movertrazo_f();
+dibujar(trazofondo);
+}

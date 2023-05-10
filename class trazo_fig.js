c1442f8b9b8b9a24fba99ef87f94ba69ae5c954f
Christian Garcia Cueva <76504626+chrisgcxo@users.noreply.github.com>
2023-05-10 20:43:45 -0300

    #arregle el fill

--- class trazo_fig.js
+++ class trazo_fig.js
@@ -4,22 +4,28 @@ class trazo_fig{
       this.tam_fig=50;
       this.posX_fig=random(this.tam_fig,windowWidth-this.tam_fig);
       this.posY_fig=windowHeight-this.tam_fig;  
-      this.vel_fig=20;
+      this.vel_fig=random(20);
       //var img de trazo//
       //this.imgt_fig=imgt_fig;//
+      this.color_fig = color(random(360), random(50, 100), random(50, 100));
     }
     //funciones y metodos//
     dibujar(){
       //image(this.imgt_fig,this.posX_fig,this.posX_fig);//
+      push();
+      pgf.noStroke();
+      pgf.fill(this.color_fig);
       pgf.ellipse(this.posX_fig,this.posY_fig,this.tam_fig,this.tam_fig);
+      pop();
     }
-    mover(){
+    mover(){  
       this.posY_fig-=this.vel_fig;
       //espacio toriodal//
       if(this.posY_fig<-50){
         this.posY_fig=windowHeight;
         this.posX_fig=random(this.tam_fig,windowWidth-this.tam_fig);
-      } 
+        this.color_fig = color(random(360), random(50, 100), random(50, 100));
+      }
     }
   }
   
